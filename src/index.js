313aa9518cc2c9d26b5f45e4ddcca30e87adcacc
Golden-midy <mwanaidimwalami@icloud.com>
2024-10-24 05:01:26 -0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,8 +42,7 @@ currentDateELement.innerHTML = formatDate(currentDate);
 function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
 
-  let display-value = document.querySelector(display-value);
-  
+  let display-value = document.querySelector("#display-value");  
   display-value.innerHTML = &{temperature};
 }
 
