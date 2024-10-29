30d201eb3b19c06c2dc4a6ce277cb4a5fd69e131
Golden-midy <mwanaidimwalami@icloud.com>
2024-10-29 05:24:28 -0900

    updated weather icon image

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ function refreshWeather(response) {
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed")
-console.log(response.data);
+let windSpeedElement = document.querySelector("#wind-speed");
+let iconElement =document.querySelector("#icon");
 
+iconElement.innerHTML = '<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />';
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = '${response.data.temperature.humidity}%';
