cc4779ffa9d196c460b7b2730e67d26e0e57dbb3
Golden-midy <mwanaidimwalami@icloud.com>
2024-10-30 03:19:55 -0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 function refreshWeather(response) {
-let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
-let iconElement =document.querySelector("#icon");
-let timeElement =document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-
-
-iconElement.innerHTML = '<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />';
-cityElement.innerHTML = response.data.city;
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = '${response.data.temperature.humidity}%';
-temperatureElement.innerHTML = Math.round(temperature);
-timeElement.innerHTML = formatDate(date);
-windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
+  iconElement.innerHTML =
+    '<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />';
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
+  temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
 }
-
-
-
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days =[
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -37,18 +33,19 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
 
-   if (minutes < 10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
-   }
-  return '${day} ${hours}:${minutes}';
+  }
+  return "${day} ${hours}:${minutes}";
 }
 
 function searchCity(city) {
-let apiKey ="1b65ca4080fbdt34boc9087362443ad0";
-let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
-axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "1b65ca4080fbdt34boc9087362443ad0";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(refreshWeather);
 }
- 
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -57,6 +54,6 @@ function handleSearchSubmit(event) {
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit)
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris")
+searchCity("Paris");
